7681fafc72c76b41a202e7202f67602f07cc09f8
Vaibhav Tripathi <vaibhavtripathi724@gmail.com>
2025-07-12 14:01:35 +0000

    upadte the src/lib/firbase.ts with this const firebaseConfig = {   apiKe

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -3,12 +3,12 @@ import { initializeApp, getApps, getApp, FirebaseApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  apiKey: "AIzaSyDfgsGJoweIaZU_5HiQny4EsKcUd9NBqn8",
+  authDomain: "ecosmart-retail.firebaseapp.com",
+  projectId: "ecosmart-retail",
+  storageBucket: "ecosmart-retail.firebasestorage.app",
+  messagingSenderId: "606450531044",
+  appId: "1:606450531044:web:ba9fb62aa3742088bdcaf1"
 };
 
 let app: FirebaseApp;
